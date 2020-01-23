46eb2c4544a80fdb5c7d2bcb2582a942e7ffbd09
addisonpolcyn <addison@airtime.com>
2020-01-23 14:22:08 -0800

    TEC-1061 changed main from returning false to 0

--- crypto/aes/aes_test.cc
+++ crypto/aes/aes_test.cc
@@ -94,7 +94,7 @@ int main() {
                                 "\x88\x99\xaa\xbb\xcc\xdd\xee\xff",
                (const uint8_t *)"\x8e\xa2\xb7\xca\x51\x67\x45\xbf"
                                 "\xea\xfc\x49\x90\x4b\x49\x60\x89")) {
-    return false;
+    return 0;
   }
 
   printf("PASS\n");
